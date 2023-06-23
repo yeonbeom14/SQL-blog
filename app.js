198d08f89874ef2d0ce9542e3b3bbb32468a0110
Yeonbeom <dusqja14@naver.com>
2023-06-23 15:45:32 +0900

    Final 게시글 수정, 삭제 API

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const indexRouter = require("./routes/index.js");
 const usersRouter = require("./routes/users.js");
 const postsRouter = require("./routes/posts.js");
 const app = express();
@@ -7,7 +8,7 @@ const PORT = 3018;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', [usersRouter, postsRouter]);
+app.use('/api', [indexRouter, usersRouter, postsRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트 번호로 서버가 실행되었습니다.');
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('블로그 백엔드 서버 입니다! GET URL 목록 : /posts, /posts/:postId');
+});
+
+module.exports = router;
